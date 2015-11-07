ed1d2488ef8f3445e3f13a6ceba0ba9007aab276
Vytautas Šaltenis <vytas@rtfb.lt>
2015-11-07 21:20:47 +0200

    Fix assignment of PID

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,7 @@ scp -q scripts/unpack.sh rtfb@rtfb.lt:/home/rtfb/unpack.sh
 scp -q package.tar.gz rtfb@rtfb.lt:/home/rtfb/package.tar.gz
 rm ./package.tar.gz
 full_path=/home/rtfb/package$suffix
-pid=ssh rtfb@rtfb.lt "pidof $full_path/rtfblog"
+pid=$(ssh rtfb@rtfb.lt "pidof $full_path/rtfblog")
 ssh rtfb@rtfb.lt "kill $pid"
 ssh rtfb@rtfb.lt "/home/rtfb/unpack.sh package$suffix"
 ssh rtfb@rtfb.lt "rm $full_path/db/dbconf.yml"
